cd1b07ffa59807d58bcc62ac39096dff04a25b24
ShobhanSrivastava <shobhansrivastava.200121067@gehu.ac.in>
2021-07-04 01:57:48 +0530

    Image function changed to Img in Image.jsx Line:4

--- components/Image.jsx
+++ components/Image.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/components/Image.module.css'
 import Image from 'next/image'
 
-export default function Image(props) {
+export default function Img(props) {
 
     return (
         <Image className={styles.image} src={props.src} alt={props.alt} />
